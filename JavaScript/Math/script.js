5114318ae8923e6edc8b54cbbfc543d5f050807f
chakhila7 <achilachiliveri189@gmail.com>
2024-04-24 15:21:17 +0530

    fork

---
+++ JavaScript/Math/script.js
@@ -0,0 +1,10 @@
+console.log("Hello")
+
+/*generate 4 digit OTP and print with console eg:1234,5678 */
+console.log(x);
+var a = +prompt("enter a value")
+var b = +prompt("enter a value")
+var c = Number(prompt("enter c value"));
+var d =(-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+var e =(-b - Math.sqrt)(b * b - 4 * a * c)) / (2 * a);
+alert("results are"+ d","+e)
---
+++ JavaScript/Strings/index.js
@@ -0,0 +1,13 @@
+console.log("Hi")
+console.log(s)
+var s = 'the kalam said "dream big"';
+var s2 ="The time is 5'o clock"
+var cost = 500;
+var str = `The cost of item is ${cost} and something ${cost} somthing`
+var str2 = "the cost of otem is "+cost+"and something"+cost+"something";
+console.log(s,s2);
+console.log(str)
+console.log(str2)
+var strr = "JavaScript"
+console.log(strr.toUpperCase())
+/* write a js code to explore all string methods */
